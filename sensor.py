7cce0849369ea329a140ae95a0956b78d55d1aac
VirtualArtificer <153398290+VirtualArtificer@users.noreply.github.com>
2023-12-10 17:51:29 -0500

    fleshed out sensor functions, added ability to init vector from iterable

--- sensor.py
+++ sensor.py
@@ -1,11 +1,75 @@
-class Sensor:
-    pass
-        # types of sensors:
-        # introspective (organism health, position etc.)
-        # entity property
-        # vector field (scents, sounds, heat, etc.)
-        # environment (time, terrain, etc.)
-        # storage (retrieves values set by previous brain simulation passes)
-        # constant
-        # oscillator (note - different powers of 10ms should be plenty, no need to have every frequency)
+from enum import Enum
+from time import time_ns
 
+class SensorType(Enum):
+    INTROSPECTIVE = 1
+    ENTITY_PROPERTY = 2
+    VECTOR_FIELD = 3
+    ENVIRONMENT = 4
+    STORAGE = 5
+    CONSTANT = 6
+    OSCILLATOR = 7
+
+
+class Sensor:
+    """
+    Base class for all sensors in the organism.
+    """
+
+    def __init__(self, sensor_type: SensorType):
+        self.sensor_type = sensor_type
+        self.relative_position = Vector()
+
+    def simulate(self, agent: "Agent"):
+        """
+        Simulates the sensor's activity for one timestep.
+
+        Args:
+            agent: The Agent instance this sensor belongs to.
+
+        Returns:
+            The sensor's output value after any filtering or transformation.
+        """
+
+        # Initial value based on sensor type
+        sensor_value = Vector()
+
+        # Introspective sensor
+        if self.sensor_type == SensorType.INTROSPECTIVE:
+            sensor_value = agent.data.get(self.query)
+
+        # Entity property sensor
+        elif self.sensor_type == SensorType.ENTITY_PROPERTY:
+            entity = resource_manager.get_entity_at_point(agent.center + self.relative_position)
+            if entity is not None:
+                sensor_value = entity.data.get(self.query)
+
+        # Vector field sensor
+        elif self.sensor_type == SensorType.VECTOR_FIELD:
+            sensor_value = resource_manager.get_field_value(self.field_type, agent.center + self.relative_position)
+
+        # Environment sensor
+        elif self.sensor_type == SensorType.ENVIRONMENT:
+            sensor_value = resource_manager.get_environment_value(self.environment_property)
+
+        # Storage sensor
+        elif self.sensor_type == SensorType.STORAGE:
+            sensor_value = agent.brain.storage.get(self.storage_key)
+
+        # Constant sensor
+        elif self.sensor_type == SensorType.CONSTANT:
+            sensor_value = self.constant_value
+
+        # Oscillator sensor
+        elif self.sensor_type == SensorType.OSCILLATOR:
+            current_time = time_ns()
+            reversed_time_string = str(current_time)[::-1]
+            sensor_value = Vector([int(digit) for digit in reversed_time_string])
+
+        # Chose not to filter or transform output, as this would hinder evolution of reactive behavior
+
+        # ...
+
+        return sensor_value
+
+    # TODO: Consider adding a note about exploring "decoding" as an intelligence test.
--- vector.py
+++ vector.py
@@ -1,4 +1,108 @@
 from typing import DefaultDict
 from collections import defaultdict
 
-Vector = DefaultDict[int, int]
+#Vector = DefaultDict[int, int]
+
+class Vector:
+    """
+    Custom Vector class for sparsely populated vectors.
+
+    Supports initialization from iterable, efficient indexing,
+    and additional features for the project.
+    """
+
+    def __init__(self, iterable=None):
+        """
+        Initializes a Vector object.
+
+        Args:
+            iterable: An optional iterable object used to populate the vector.
+        """
+        self.data = defaultdict(int)  # stores values with index as key
+        self.max_index = -1  # tracks the highest populated index
+
+        if iterable is not None:
+            for index, value in enumerate(iterable):
+                self.__setitem__(index, value)
+
+    def __getitem__(self, index):
+        """
+        Retrieves the value at a specific index.
+
+        Args:
+            index: The index of the element to retrieve.
+
+        Returns:
+            The element value at the specified index.
+            If the index is not populated, returns 0.
+        """
+        if index > self.max_index:
+            return 0
+        return self.data[index]
+
+    def __setitem__(self, index, value):
+        """
+        Sets the value at a specific index.
+
+        Args:
+            index: The index of the element to set.
+            value: The value to assign to the element.
+        """
+        self.data[index] = value
+        self.max_index = max(self.max_index, index)
+
+    def __len__(self):
+        """
+        Returns the logical size of the vector.
+
+        Returns:
+            The highest populated index + 1.
+        """
+        return self.max_index + 1
+
+    # Additional features for the project
+
+    def to_bytes(self):
+        """
+        Converts the vector data to a byte stream.
+
+        Returns:
+            A byte array representing the vector data.
+        """
+        # ... implement logic to convert vector data to bytes
+        # consider using pickle or other serialization library
+        # ...
+        return bytes
+
+    def from_bytes(self, data):
+        """
+        Reconstructs the vector from a byte stream.
+
+        Args:
+            data: A byte array containing the vector data.
+        """
+        # ... implement logic to reconstruct vector data from bytes
+        # consider using pickle or other deserialization library
+        # ...
+        pass
+
+    def get_non_zero_indices(self):
+        """
+        Returns a list of all non-zero indices in the vector.
+
+        Returns:
+            A list of integers representing the non-zero indices.
+        """
+        return list(self.data.keys())
+
+    def apply_function(self, func):
+        """
+        Applies a function to all elements of the vector.
+
+        Args:
+            func: A function that takes a single element as input and returns a new value.
+        """
+        for index, value in self.data.items():
+            self.data[index] = func(value)
+
+    # ... add other relevant methods as needed ...
